52826e4c565636fa68506df2352eef3f85a2e315
Michael Fabian Dirks <info@xaymar.com>
2017-03-04 16:11:20 +0100

    InvSqrt: More CMake fixes

--- InvSqrt_Double/InvSqrt_Double.cpp
+++ InvSqrt_Double/InvSqrt_Double.cpp
@@ -1,8 +1,6 @@
 // InvSqrt_Double.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
 #include <iostream>
 #include <random>
 
--- InvSqrt_Single/InvSqrt_Single.cpp
+++ InvSqrt_Single/InvSqrt_Single.cpp
@@ -1,7 +1,6 @@
 // FloatMathSpeedTest.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <chrono>
 #include <vector>
 #include <tuple>
