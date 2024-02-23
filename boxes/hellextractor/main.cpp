48eef66ff94253afb74509731c55341c527ff5c8
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2024-02-23 08:35:11 +0100

    hellextractor: Discovered U,V for vertex28_t

--- boxes/hellextractor/main.cpp
+++ boxes/hellextractor/main.cpp
@@ -28,6 +28,56 @@ struct shared_ptr_with_deleter : public std::shared_ptr<T> {
 	}
 };
 
+struct half {
+	uint16_t value;
+
+	half()
+	{
+		operator=(0.0f);
+	}
+	half(float v)
+	{
+		operator=(v);
+	}
+	half(uint16_t v)
+	{
+		operator=(v);
+	}
+
+	half& operator=(float v)
+	{
+		auto as_uint = [](const float x) { return *(uint32_t*)&x; };
+
+		// IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+		const uint32_t b = as_uint(v) + 0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
+		const uint32_t e = (b & 0x7F800000) >> 23; // exponent
+		const uint32_t m = b & 0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+		value            = (b & 0x80000000) >> 16 | (e > 112) * ((((e - 112) << 10) & 0x7C00) | m >> 13) | ((e < 113) & (e > 101)) * ((((0x007FF000 + m) >> (125 - e)) + 1) >> 1) | (e > 143) * 0x7FFF; // sign : normalized : denormalized : saturate
+	}
+
+	half& operator=(uint16_t v)
+	{
+		value = v;
+	}
+
+	operator float() const
+	{
+		auto as_float = [](const uint32_t x) { return *(float*)&x; };
+		auto as_uint  = [](const float x) { return *(uint32_t*)&x; };
+
+		// IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
+		const uint32_t e = (value & 0x7C00) >> 10; // exponent
+		const uint32_t m = (value & 0x03FF) << 13; // mantissa
+		const uint32_t v = as_uint((float)m) >> 23; // evil log2 bit hack to count leading zeros in denormalized format
+		return as_float((value & 0x8000) << 16 | (e != 0) * ((e + 112) << 23 | m) | ((e == 0) & (m != 0)) * ((v - 37) << 23 | ((m << (150 - v)) & 0x007FE000))); // sign : normalized : denormalized
+	}
+
+	operator uint16_t() const
+	{
+		return value;
+	}
+};
+
 #ifdef WIN32
 #include <Windows.h>
 
@@ -154,20 +204,21 @@ namespace hd2 {
 	};
 
 	struct vertex20_t {
-		float x, y, z;
-		float u, v;
+		float    x, y, z;
+		uint32_t __unk00[2];
 	};
 
 	struct vertex28_t {
-		float x, y, z;
-		float nx, ny;
-		float u, v;
+		float    x, y, z;
+		uint32_t __unk00;
+		half     u, v;
+		half     nx, ny;
+		uint32_t __unk01;
 	};
 
 	struct vertex36_t {
-		float x, y, z;
-		float nx, ny, nz;
-		float u, v, w;
+		float    x, y, z;
+		uint32_t __unk00[6];
 	};
 
 	struct lodinfo_t {
@@ -357,15 +408,14 @@ int main(int argc, const char** argv)
 				hd2::vertex20_t const* vtx = reinterpret_cast<decltype(vtx)>(vtx_addr);
 				output << "# " << i << std::endl;
 				output << "v " << vtx->x * hd2::mesh_scale << " " << vtx->y * hd2::mesh_scale << " " << vtx->z * hd2::mesh_scale << std::endl;
-				output << "vt " << vtx->u << " " << vtx->v << std::endl;
+
 				break;
 			}
 			case sizeof(hd2::vertex28_t): {
 				hd2::vertex28_t const* vtx = reinterpret_cast<decltype(vtx)>(vtx_addr);
 				output << "# " << i << std::endl;
 				output << "v " << vtx->x * hd2::mesh_scale << " " << vtx->y * hd2::mesh_scale << " " << vtx->z * hd2::mesh_scale << std::endl;
-				output << "vn " << vtx->nx << " " << vtx->ny << " " << FLT_EPSILON << std::endl;
-				output << "vt " << vtx->u << " " << vtx->v << std::endl;
+				output << "vt " << (float)vtx->u << " " << (float)vtx->v << std::endl;
 
 				break;
 			}
@@ -373,8 +423,6 @@ int main(int argc, const char** argv)
 				hd2::vertex36_t const* vtx = reinterpret_cast<decltype(vtx)>(vtx_addr);
 				output << "# " << i << std::endl;
 				output << "v " << vtx->x * hd2::mesh_scale << " " << vtx->y * hd2::mesh_scale << " " << vtx->z * hd2::mesh_scale << std::endl;
-				output << "vn " << vtx->nx << " " << vtx->ny << " " << vtx->nz << std::endl;
-				output << "vt " << vtx->u << " " << vtx->v << " " << vtx->w << std::endl;
 
 				break;
 			}
@@ -384,13 +432,17 @@ int main(int argc, const char** argv)
 		uint8_t const* idx_max_addr = mesh_ptr + lod.index_offset + lod.index_size;
 		uint8_t const* idx_addr     = mesh_ptr + lod.index_offset + mesh.indices_offset;
 		for (size_t i = 0; i < mesh.indices_count; i += 3, idx_addr += sizeof(hd2::index_t) * 3) {
-			hd2::index_t const* idx0     = reinterpret_cast<decltype(idx0)>(idx_addr);
-			hd2::index_t const* idx1     = reinterpret_cast<decltype(idx1)>(idx_addr + 2);
-			hd2::index_t const* idx2     = reinterpret_cast<decltype(idx2)>(idx_addr + 4);
+			hd2::index_t const* idx0 = reinterpret_cast<decltype(idx0)>(idx_addr);
+			hd2::index_t const* idx1 = reinterpret_cast<decltype(idx1)>(idx_addr + sizeof(hd2::index_t));
+			hd2::index_t const* idx2 = reinterpret_cast<decltype(idx2)>(idx_addr + sizeof(hd2::index_t) * 2);
 
 			output << "# " << i << std::endl;
-			output << "f " << (1 + idx0->index) << " " << (1 + idx1->index) << " " << (1 + idx2->index) << std::endl;
-		} // This breaks past the lod.index_size "boundary", but the export is correct.
+			output << "f ";
+			output << (1 + idx0->index) << "/" << (1 + idx0->index) << "/" << (1 + idx0->index) << " ";
+			output << (1 + idx1->index) << "/" << (1 + idx1->index) << "/" << (1 + idx1->index) << " ";
+			output << (1 + idx2->index) << "/" << (1 + idx2->index) << "/" << (1 + idx2->index) << std::endl;
+			//output << "f " << (1 + idx0->index) << " " << (1 + idx1->index) << " " << (1 + idx2->index) << std::endl;
+		}
 
 		output.close();
 	}
